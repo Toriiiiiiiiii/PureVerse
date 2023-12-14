df875b2f92bc9543d716e80a444fd4ebf747f435
Tori Alex Hall <toriahall7@gmail.com>
2023-12-14 14:25:07 +0000

    Made the program actually compile -> Implemented functions related to  structure.

--- src/ast.c
+++ src/ast.c
@@ -42,3 +42,49 @@ void printASTNode(pv_astnode_t node) {
   printNodeIndented(node, 0);
   printf("\n");
 }
+
+pv_astnodelist_t createNodeList() {
+  pv_astnodelist_t result = {
+    .capacity = 10,
+    .size = 0,
+    .head = (pv_astnode_t *)malloc(10 * sizeof(pv_astnode_t))
+  };
+
+  return result;
+}
+
+void destroyNodeList(pv_astnodelist_t *list) {
+  list->capacity = 0;
+  free(list->head);
+}
+
+void __reallocateNodeList(pv_astnodelist_t *list, int newsize) {
+  list->head = realloc(list->head, newsize * sizeof(pv_astnode_t));
+  list->capacity = newsize;
+}
+
+void appendNodeList(pv_astnodelist_t *list, pv_astnode_t node) {
+  if(list->size >= list->capacity / 2) __reallocateNodeList(list, list->capacity * 2);
+
+  list->head[list->size] = node;
+  list->size++;
+}
+
+pv_astnode_t popNodeList(pv_astnodelist_t *list) {
+  if(list->size < list->capacity / 2) __reallocateNodeList(list, list->capacity / 2);
+
+  list->size--;
+  return list->head[list->size];
+}
+
+void setNodeListIndex(pv_astnodelist_t *list, int index, pv_astnode_t node) {
+  if(index >= list->size) return;
+
+  list->head[index] = node;
+}
+
+pv_astnode_t getNodeListIndex(pv_astnodelist_t *list, int index) {
+  if(index >= list->size) return (pv_astnode_t){0};
+
+  return list->head[index];
+}
